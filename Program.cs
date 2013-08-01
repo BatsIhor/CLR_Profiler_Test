022693913e194f9cb356480e5d5569b607972801
Ihor Bats <batsihor@gmail.com>
2013-08-01 23:25:02 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CLR_Profiler_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                MySimpleType obj = new MySimpleType();
+            }
+
+            List<MySimpleType> ints = new List<MySimpleType>();
+            for (int i = 0; i < 100000; i++)
+            {
+                ints.Add(new MySimpleType());
+            }
+
+            int count = ints.Count;
+
+
+            Thread.Sleep(1000);
+
+            for (int i = 0; i < count; i++)
+            {
+                ints[i] = ints[i];
+            }
+        }
+    }
+
+    class MySimpleType
+    {
+        public MySimpleType()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                object obj = new object();
+            }
+        }
+    }
+}
